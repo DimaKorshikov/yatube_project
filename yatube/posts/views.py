2118959bd20c8fc0a1d6b7a91bb686bc713cf467
DimaKorshikov <pennyvaiz@gmail.com>
2021-07-22 22:07:26 +0300

    Загрузка тестовых данных

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     return HttpResponse(f'Список постов {slug}')
+
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+    return render(request, template, context)
 
 
