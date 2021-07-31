b929daee7221a424d719a0e77a2d2bff6de72909
DimaKorshikov <pennyvaiz@gmail.com>
2021-07-31 23:26:39 +0300

    Первая проверка

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    # Перечисляем поля, которые должны отображаться в админке
-    list_display = ('pk', 'text', 'pub_date', 'author')
-    # Добавляем интерфейс для поиска по тексту постов
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
+    list_editable = ('group',)
     search_fields = ('text',)
-    # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
@@ -16,3 +20,4 @@ class PostAdmin(admin.ModelAdmin):
 
 # класс PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
